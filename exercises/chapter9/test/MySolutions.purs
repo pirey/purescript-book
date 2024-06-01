6ce602ad1356789e2736427c41b6cede12e91c56
Yeri <yeri@accelbyte.net>
2024-06-01 23:24:58 +0700

    chapter9: 1

--- exercises/chapter9/test/MySolutions.purs
+++ exercises/chapter9/test/MySolutions.purs
@@ -2,4 +2,14 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Effect.Aff (Aff)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile, writeTextFile)
+import Node.Path (FilePath)
+
+concatenateFiles :: FilePath -> FilePath -> FilePath -> Aff Unit
+concatenateFiles in1 in2 out = do
+  content1 <- readTextFile UTF8 in1
+  content2 <- readTextFile UTF8 in2
+  writeTextFile UTF8 out (content1 <> content2)
+  pure unit
